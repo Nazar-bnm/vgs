bc4bf06ab010c2ace5e29edcdb1c56e189d086f3
Nazar-bnm <nazar.bnm@gmail.com>
2017-05-16 18:24:54 +0300

    add accordion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(document).ready(function() {
+  $('.panel-collapse').on('show.bs.collapse', function () {
+    console.log('open');
+    $(this).parents('.card_accordion').addClass('active dropup');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    console.log('hide');
+    $(this).parents('.card_accordion').removeClass('active dropup');
+  });
+});
