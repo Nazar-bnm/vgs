c20dcee9ce45eac52c3e470f8bbfa76d47b1aa9e
Nazar-bnm <nazar.bnm@gmail.com>
2017-05-17 14:29:26 +0300

    reduce active card and change open-arrows

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
   $('.panel-collapse').on('show.bs.collapse', function () {
     console.log('open');
     $(this).parents('.card_accordion').addClass('active dropup');
+    $(this).parents('.card_accordion').find('.fa').removeClass('fa-angle-down');
+    $(this).parents('.card_accordion').find('.fa').addClass('fa-angle-up');
   });
 
   $('.panel-collapse').on('hide.bs.collapse', function () {
     console.log('hide');
     $(this).parents('.card_accordion').removeClass('active dropup');
+    $(this).parents('.card_accordion').find('.fa').removeClass('fa-angle-up');
+    $(this).parents('.card_accordion').find('.fa').addClass('fa-angle-down');
   });
 });
